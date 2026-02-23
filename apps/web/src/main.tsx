f693cfdcb5637363c8257633b255e5e635f895ac
Samer Aamar <saaamar@microsoft.com>
2026-02-23 10:45:32 +0200

    fix icon 404 error

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -9,6 +9,18 @@ import { LocationProvider } from './location/LocationContext';
 import { MethodProvider } from './method/MethodContext';
 
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// Fix Leaflet default marker icon paths broken by bundlers (Vite/Webpack).
+delete (L.Icon.Default.prototype as Record<string, unknown>)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+});
 
 initI18n('en');
 
@@ -16,7 +28,10 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LocationProvider>
       <MethodProvider>
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <BrowserRouter
+          basename={import.meta.env.BASE_URL}
+          future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+        >
           <App />
         </BrowserRouter>
       </MethodProvider>
